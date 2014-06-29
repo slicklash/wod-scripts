e31a31d5adc24ae9f411a14d11e1502f9045684a
slicklash <slicklash@gmail.com>
2014-06-30 00:50:14 +0300

    [Adventure Assistant] initial version

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ wod-scripts
 
 Collection of scripts for [World-of-Dungeons](http://www.world-of-dungeons.net)
 
-favorite_menu.user.js       - Allows to customizable main menu
+adventure_assistant.user.js - Allows to do adventures using keyboard
+
+favorite_menu.user.js       - Allows to customize main menu
 
 hero_list.user.js           - Adds functionality to order your heroes
 
@@ -12,5 +14,3 @@ profile_export.user.js      - Adds functionality to export profile information t
 storage_management.user.js  - Adds additional functionality for group storage and treasure vault management
 
 tidy_trade.user.js          - Sorts trade items and calculates total sum of uses
-
-
---
+++ release/adventure_assistant.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name           Adventure Assistant
+// @description    Script allows to do adventures using keyboard
+// @version        1.0
+// @author         Never
+// @include        http*://*.world-of-dungeons.net/wod/spiel/event/play.php*
+// @include        http*://*.world-of-dungeons.net/wod/spiel/event/eventlist.php*
+// ==/UserScript==
+
+(function(window, undefined) {
+
+var buttons = document.querySelectorAll('a.button, input[type="submit"]'), choices = document.querySelectorAll('input[type="radio"]'), choice, button, label, choiceMap = {}, key, i;
+
+for (i = 0; i < buttons.length; i++) {
+    button = buttons[i];
+    if (button.innerHTML === "Next" || (button.value && button.value.trim() === 'Ok')) {
+        button.focus();
+        break;
+    }
+}
+
+for (i = 0; i < choices.length; i++) {
+    choice = choices[i];
+    label = choice.parentNode;
+    key = i + 49;
+    choiceMap[key] = i;
+    if (label) {
+        label.innerHTML += '<span><sup style="color: #55f">' + (i + 1) + '</sup></span>';
+    }
+}
+
+document.onkeydown = function (e) {
+    key = e.which;
+    if (choiceMap.hasOwnProperty(key)) {
+        choice = document.querySelectorAll('input[type="radio"]')[choiceMap[key]];
+        choice.checked = true;
+        choice.focus();
+        return false;
+    }
+};
+})();
