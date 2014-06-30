35a0fc6045462776e4fb01eb98489814eb8c2566
slicklash <slicklash@gmail.com>
2014-06-30 09:48:16 +0300

    -a

--- src/adventure_assistant/header.js
+++ src/adventure_assistant/header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Adventure Assistant
 // @description    Script allows to do adventures using keyboard
-// @version        1.0
+// @version        1.0.1
 // @author         Never
 // @include        http*://*.world-of-dungeons.net/wod/spiel/event/play.php*
 // @include        http*://*.world-of-dungeons.net/wod/spiel/event/eventlist.php*
