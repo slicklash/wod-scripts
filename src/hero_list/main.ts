5cc90aaa8733c970c69ecf3b7b2868f41196c65c
slicklash <slicklash@gmail.com>
2016-05-24 22:42:29 +0300

    [hero_list] show dungeon name

--- src/hero_list/main.ts
+++ src/hero_list/main.ts
@@ -18,11 +18,12 @@ function main() {
                 let row    = rows[i],
                     cells  = row.cells,
                     hid    = Number(cells[0].querySelector('input').value).toString(),
-                    weight = Number(cells[5].querySelector('input').value);
+                    weight = Number(cells[5].querySelector('input').value),
+                    group  = 'no group';
 
                 if (isNaN(weight)) weight = 0;
 
-                GM_setValue(hid, weight);
+                GM_setValue(hid, JSON.stringify({ weight, group }));
             }
 
             var form = (<any>document.forms).the_form;
@@ -40,7 +41,7 @@ function main() {
             buttonSave = add('input', headerWeight);
 
         label.innerHTML = 'weight<br/>';
-        attr(buttonSave, { 'type': 'button', 'value': 'Save', 'class': 'button clickable' });
+        attr(buttonSave, { 'type': 'button', 'value': 'Update', 'class': 'button clickable' });
         buttonSave.addEventListener('click', saveWeights, false);
         rows[0].appendChild(headerWeight);
         newTbody.appendChild(rows[0]);
@@ -49,11 +50,23 @@ function main() {
 
         for (let i = 1; i < rows.length; i++) {
 
-            let row    = rows[i],
-                cells  = row.cells,
-                hid    = Number(cells[0].querySelector('input').value).toString(),
-                level  = Number(textContent(cells[2])),
-                weight = (GM_getValue(hid) || (level === 0 ? 100 : level));
+            let row     = rows[i],
+                cells   = row.cells,
+                hid     = Number(cells[0].querySelector('input').value).toString(),
+                level   = Number(textContent(cells[2])),
+                next    = cells[4],
+                tooltip = next.getAttribute('onmouseover'),
+                time    = textContent(cells[4]).trim(),
+                value   = GM_getValue(hid),
+                info    = value ? JSON.parse(value) : {},
+                weight  = info.weight || (level === 0 ? 100 : level);
+
+            if (tooltip) {
+                let dungeon = tooltip.split("'")[1];
+                let localTime = time.split('/')[0];
+                textContent(next, `${localTime} - ${dungeon}`);
+                next.setAttribute('onmouseover', `return wodToolTip(this,"${dungeon} <br/> ${time}");`);
+            }
 
             heroes.push({ 'weight': Number(weight), 'row': row });
         }
